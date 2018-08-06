b2696dcd34854f494ce343f4d8ed5e1076d9abb9
Gina Lee <ginalee1114@gmail.com>
2018-08-06 00:30:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,30 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data.each do |season_num, season_data|
+    if season_num == season 
+      season_data.each do |contestant_info|
+        if contestant_info["status"] == "Winner"
+          winner = contestant_info["name"].split
+        end 
+      end 
+    end 
+  end 
+  winner.pop
+  winner.join("")
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_num, season_data|
+    season_data.each do |contestant_info|
+      if contestant_info["occupation"] == occupation
+        return contestant_info["name"]
+      end 
+    end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
 end
 
 def get_occupation(data, hometown)
